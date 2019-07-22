60fdb57b851713c8654fffa136c3904f6cf0353a
YuriiTovmach <portfolio_yurii_tovmach@ukr.net>
2019-07-22 17:36:53 +0300

    second commit

--- factorial.py
+++ factorial.py
@@ -1,7 +1,18 @@
-number = int(input("введите число:"))
-i = 1
+#factorial with while
+
+# number = int(input("введите число:"))
+# i = 1
+# factorial = 1
+# while i <=number:
+#     factorial *= i
+#     i +=1
+# print("факториал числа", number, "равен", factorial)
+
+
+# factorial with for
+
+number = int(input("Введите число: "))
 factorial = 1
-while i <=number:
+for i in range(1, number+1):
     factorial *= i
-    i +=1
-print("факториал числа", number, "равен", factorial)
+print("Факториал числа", number, "равен", factorial)
